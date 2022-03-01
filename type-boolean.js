dbe25ffddfad9e747f525c481875078630c025e2
Lucas Marcondes <lucas.marcondes36@gmail.com>
2022-03-01 12:11:56 -0300

    Criando arquivo type-boolean.js

---
+++ type-boolean.js
@@ -0,0 +1,15 @@
+//  Digitar no terminal o comando { node type-boolean.js } para executar o código.
+// Boolean
+// true -> verdadeiro
+// false - falso
+
+const primeiroNumero = 5;
+const segundoNumero = 5;
+let cadastroAtivado = true;
+
+console.log(primeiroNumero === segundoNumero);
+
+const texto1 = "Alura";
+const texto2 = "alura";
+
+console.log(texto1 === texto2);
--- type-string.js
+++ type-string.js
@@ -1,3 +1,5 @@
+//  Digitar no terminal o comando { node type-string.js } para executar o código.
+
 const texto1 = "Olá, mundo!";
 const texto2 = 'Olá, mundo!';
 const senha = "senhaSuperSegura456";
