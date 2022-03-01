1afd468012abf95ac7954b9f710b3138ea277f41
Lucas Marcondes <lucas.marcondes36@gmail.com>
2022-03-01 11:41:45 -0300

    Criando arquivo type-number.js

---
+++ type-number.js
@@ -0,0 +1,25 @@
+//  Digitar no terminal o comando { node type-number } para executar o código.
+
+// Tipo Number
+
+const num1 = 1;
+const num2 = 2;
+
+const operacaomatematica = num1 + num2;
+
+console.log(operacaomatematica);
+
+// Ponto Flutuante
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5;
+
+const novaOperacao = numeroPontoFlutuante / pontoFlutuanteSemZero;
+
+console.log(novaOperacao);
+
+// NaN (Not a Number) - Não é um número
+
+const alura = "Alura";
+console.log(alura * num1);
+
